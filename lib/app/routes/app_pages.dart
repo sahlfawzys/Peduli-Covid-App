1fef46acbbda082366783c3ac43f9ca786ee28af
Sahl Fawzy Sutopo <sahlfawzys@gmail.com>
2022-05-19 17:26:13 +0700

    add app interface images

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -14,7 +14,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.MAIN_PAGE;
+  static const INITIAL = Routes.LOGIN;
 
   static final routes = [
     GetPage(
